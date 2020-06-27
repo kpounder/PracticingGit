bafe122bec05af7528c560d36fb8cde386838679
Kyle Pounder <k.pounder@gmail.com>
2020-06-27 12:49:38 -0700

    Added mulitply_by_two function

--- hello_world.py
+++ hello_world.py
@@ -8,3 +8,10 @@ print('Fifth hello')
 def print_n_times(word):
     for i in range(n):
         print(word)
+
+
+def multiply_by_two(num):
+    try:
+        print(num * 2)
+    except Exception:
+        print('Input must be number.')
